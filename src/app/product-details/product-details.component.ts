1b0350e7c95f0462c5139cf5f8992a65d4a40735
GitLupus21 <mobil18@schmidt-oewi.de>
2020-06-28 23:39:48 +0200

    cart component filled

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -24,6 +24,5 @@ export class ProductDetailsComponent implements OnInit {
 
   addToCart(product) {
     this.cartService.addToCart(product);
-    window.alert("Your product has been added to the cart!");
   }
 }
